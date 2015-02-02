1083b78e259b6bc62344fce9239cc7d093383e2a
alexmartinssi <alexmbezerrajp@gmail.com>
2015-02-02 13:48:18 -0300

    Add login invalido.

--- SAVCM/settings.py
+++ SAVCM/settings.py
@@ -12,6 +12,12 @@ GESTOR_SENHA = 'abc123'
 USUARIO_LOGIN = 'usuario'
 USUARIO_SENHA = 'abc123'
 
+INVALIDO_LOGIN = 'admin_errado'
+SENHA = 'abc123'
+
+LOGIN = 'admin'
+INVALIDA_SENHA = 'abc123_errada'
+
 #Palavras reservadas do Robot Framework
 DELAY = 0.2
 
