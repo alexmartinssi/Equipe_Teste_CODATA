12ec93369809539210e16530b90912a3c29bad83
makoby <makoby@gmail.com>
2015-03-18 10:18:50 -0300

    alteracao do setings

--- SAVCM/settings.py
+++ SAVCM/settings.py
@@ -1,10 +1,11 @@
 #Pagina do sistema a ser testado
 
-PAGINA = 'http://prxcodata.pb.gov.br/SAVCM/login.jsf'
+#PAGINA = 'http://prxcodata.pb.gov.br/SAVCM/login.jsf'
+PAGINA = 'http://localhost:8080/SAVCM/login.jsf'
 
 #Login e senha por perfil de usuario
-ADMIN_LOGIN = 'makoby'
-ADMIN_SENHA = 'makoby'
+ADMIN_LOGIN = 'admin'
+ADMIN_SENHA = 'abc123'
 
 GESTOR_LOGIN = 'gestor'
 GESTOR_SENHA = 'abc123'
